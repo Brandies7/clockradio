1a180fe50ac4b02fd93eb784b268962540bb6bc6
Brandies7 <adanb82@gmail.com>
2017-08-06 20:08:19 -0500

    clockradio complete

---
+++ ClockRadio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RadioClock useRadio = new RadioClock();
+            useRadio.turnOn();
+            useRadio.getTime();
+            useRadio.radioStation();
+            
+            Console.ReadLine();
+           
+
+        }
+    }
+}
---
+++ ClockRadio/RadioClock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class RadioClock
+    {
+        public double time;
+        public string isOn;
+        public double station;
+
+        public RadioClock()
+        {
+
+        }
+
+        public double getTime()
+        {
+            Console.WriteLine("Enter alarm time");
+            time = Convert.ToDouble(Console.ReadLine());
+            return time;
+        }
+
+        public void turnOn()
+        {
+            Console.WriteLine("Turn alarm on: 'yes' or 'no'");
+            isOn = Console.ReadLine();
+        }
+
+        public double radioStation()
+        {
+            Console.WriteLine("What station would you like to listen to?");
+            station = Convert.ToDouble(Console.ReadLine());
+            return station;
+        }
+
+    }    
+    
+}
+
+
+
+
+
+    
+
